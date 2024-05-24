c60721e0ae38eedeeeb77c789afbc43739c4e586
HLong <hailongkc1203@gmail.com>
2024-05-25 04:48:41 +0700

    add new appointment to array

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,],
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
 export class AppointmentListComponent {
 
-  appointment: Appointment = {
-    id: 1,
-    title: 'hai long',
-    date: new Date('2024-05-25')
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+  appointment: Appointment[] = [];
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointment.push(newAppointment);
+      this.newAppointmentDate = new Date();
+      this.newAppointmentTitle = "";
+      console.log(newAppointment);
+
+    }
   }
+
 }
